39867c7be7825b682ab3ba790825ca91c6df0616
harshthummar27 <harshthummar542@gmail.com>
2026-05-07 11:01:51 +0530

    minor change

--- controllers/customerController.js
+++ controllers/customerController.js
@@ -837,6 +837,7 @@ exports.customerCommissionList = async (req, res) => {
   try {
     const customers = await Customer.find({ verifyStatus: 'verified' })
       .populate('assignToContractor', 'fullName email')
+      .populate('user_id', 'id fullName')
       .populate({
         path: 'commissions.salesPerson',
         model: 'User',
@@ -892,8 +893,8 @@ exports.customerCommissionList = async (req, res) => {
         id: customer._id,
         name: customer.name,
         company: customer.company,
-        salesPerson: customer.salesPerson, // This is the string from Customer model
-        contractor: customer.assignToContractor?.fullName || '', // This is the populated contractor
+        salesPerson: customer.user_id?.fullName || '',
+        contractor: customer.assignToContractor?.fullName || '',
 
         total_overall_amount: customerTotal,
         total_paid_amount: customerPaid,
